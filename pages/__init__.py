f194b7bffeaeca14a7cef96bf5a826482c853c9a
Vasiliy Yamutin <iamutin@yandex.ru>
2025-04-05 13:25:49 +0300

    Знакомство с PageObject. Практика работы с Page Object

---
+++ pages/__init__.py
@@ -0,0 +1,11 @@
+__all__ = [
+    "BasePage",
+    "DashboardPage",
+    "LoginPage",
+    "RegistrationPage",
+]
+
+from .base_page import BasePage
+from .dashboard_page import DashboardPage
+from .login_page import LoginPage
+from .registration_page import RegistrationPage
